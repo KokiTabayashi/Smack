1d539f0a61839cf21c7d0fdb418a5b6c0ce9b2a3
Koki Tabayashi <koki@K-MacBook-Pro.local>
2018-04-08 20:49:58 -0700

    lesson 22

---
+++ Smack/Services/SocketService.swift
@@ -0,0 +1,53 @@
+//
+//  SocketService.swift
+//  Smack
+//
+//  Created by Koki Tabayashi on 2018/04/08.
+//  Copyright © 2018年 Koki Tabayashi. All rights reserved.
+//
+
+import UIKit
+import SocketIO
+
+class SocketService: NSObject {
+    
+    static let instance = SocketService()
+    
+    override init() {
+        super.init()
+    }
+    
+//    var socket: SocketIOClient = SocketIOClient(socketURL: URL(string: BASE_URL)!)
+//    let manager = SocketManager(socketURL: URL(string: BASE_URL)!, config: [.log(true), .compress])
+    let manager = SocketManager(socketURL: URL(string: BASE_URL)!)
+    lazy var socket: SocketIOClient = manager.defaultSocket
+    
+    func establishConnection() {
+        socket.connect()
+//        manager.defaultSocket.connect()
+    }
+    
+    func closeConnection() {
+        socket.disconnect()
+//        manager.defaultSocket.disconnect()
+    }
+    
+    func addChannel(channelName: String, channelDescription: String, completion: @escaping CompletionHandler) {
+        socket.emit("newChannel", channelName, channelDescription)
+//        manager.defaultSocket.emit("newChannel", channelDescription)
+        completion(true)
+    }
+    
+    func getChannel(completion: @escaping CompletionHandler) {
+        socket.on("channelCreated") { (dataArray, ack) in
+//        manager.defaultSocket.on("channelCreated") { (dataArray, ack) in
+            guard let channelName = dataArray[0] as? String else { return }
+            guard let channelDesc = dataArray[1] as? String else { return }
+            guard let channelId = dataArray[2] as? String else { return }
+            
+            let newChannel = Channel(channelTitle: channelName, channelDescription: channelDesc, id: channelId)
+            MessageService.instance.channels.append(newChannel)
+            completion(true)
+        }
+    }
+}
